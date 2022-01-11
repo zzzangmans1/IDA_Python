4b9e5fa6f4b7c236bac565efe0cf28775e5b815e
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2022-01-11 23:34:29 +0900

    함수 목록 출력

--- output_func_list.py
+++ output_func_list.py
@@ -1,3 +1,5 @@
 # 함수 목록 출력
+print("*** 함수 목록 출력 시작 ***")
 for func in Functions(): # 함수들 하나씩 가져와 func에 넣는다.
     print(hex(func)+ " : "+idc.get_func_name(func)) # 주소값으로 출력하고 함수 이름 으로출력
+print("*** 함수 목록 출력 끝 ***")
