ab3b4d505418533d9bc95e13d28ab1300342fc3e
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2022-01-12 20:55:42 +0900

    섹션 출력

--- print_section.py
+++ print_section.py
@@ -1,6 +1,6 @@
 print("*** 섹션 출력 시작 ***")
 print(" name\tstart\tend ")
 for seg in Segments():
-    print(idc.get_segm_name(seg), idc.get_segm_start(seg), idc.get_segm_end(seg))
+    print(idc.get_segm_name(seg), hex(idc.get_segm_start(seg)), hex(idc.get_segm_end(seg)))
 print("*** 섹션 출력 끝 ***")
     
